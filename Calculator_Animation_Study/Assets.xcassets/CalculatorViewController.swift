a32cb4d98ac16a65b84a5f00414648d574049215
정채은 <cce523@hanmail.net>
2023-06-12 22:09:18 +0900

    Feat [#1] 컴포넌트 생성

--- Calculator_Animation_Study/Assets.xcassets/CalculatorViewController.swift
+++ Calculator_Animation_Study/Assets.xcassets/CalculatorViewController.swift
@@ -7,23 +7,77 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+import SnapKit
+import SwiftyColor
+import Then
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class CalculatorViewController: UIViewController {
+    
+    private lazy var ACButton = UIButton().then {
+        $0.setTitle("AC", for: .normal)
+        $0.titleLabel?.textColor = .black
+        $0.titleLabel?.font = .systemFont(ofSize: 35, weight: UIFont.Weight(rawValue: 500))
+        $0.backgroundColor = 0xA5A5A5.color
+        $0.layer.cornerRadius = 0.5 * $0.bounds.size.width
+        $0.clipsToBounds = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private lazy var signedButton = UIButton().then {
+        $0.setImage(UIImage(named:"signed"), for: .normal)
+        $0.backgroundColor = 0xA5A5A5.color
+        $0.layer.cornerRadius = 0.5 * $0.bounds.size.width
+        $0.clipsToBounds = true
     }
-    */
+    
+    private lazy var persentButton = UIButton().then {
+        $0.setTitle("%", for: .normal)
+        $0.titleLabel?.textColor = .black
+        $0.titleLabel?.font = .systemFont(ofSize: 40, weight: UIFont.Weight(rawValue: 400))
+        $0.backgroundColor = 0xA5A5A5.color
+        $0.layer.cornerRadius = 0.5 * $0.bounds.size.width
+        $0.clipsToBounds = true
+    }
+    
+    private lazy var divideButton = signButton(text: "÷")
+    private lazy var multiplyButton = signButton(text: "×")
+    private lazy var minusButton = signButton(text: "-")
+    private lazy var plusButton = signButton(text: "+")
+    private lazy var equalButton = signButton(text: "=")
 
+    private lazy var number1Button = numberButton(text: "1")
+    private lazy var number2Button = numberButton(text: "2")
+    private lazy var number3Button = numberButton(text: "3")
+    private lazy var number4Button = numberButton(text: "4")
+    private lazy var number5Button = numberButton(text: "5")
+    private lazy var number6Button = numberButton(text: "6")
+    private lazy var number7Button = numberButton(text: "7")
+    private lazy var number8Button = numberButton(text: "8")
+    private lazy var number9Button = numberButton(text: "9")
+    private lazy var dotButton = numberButton(text: ".")
+    
+    private lazy var number0Button = UIButton().then {
+        $0.setTitle("0", for: .normal)
+        $0.titleLabel?.textColor = .white
+        $0.titleLabel?.font = .systemFont(ofSize: 39, weight: UIFont.Weight(rawValue: 400))
+        $0.backgroundColor = 0x333333.color
+        $0.layer.cornerRadius = 50
+        $0.clipsToBounds = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+    
+}
+
+extension CalculatorViewController {
+    private func setUI() {
+        view.backgroundColor = .black
+    }
+    
+    private func setLayout() {
+        
+    }
 }
