affcb9ad9fc65a73b69c5d1f1125202898e1f3a4
정채은 <cce523@hanmail.net>
2023-06-12 22:10:33 +0900

    Add [#1] UIView extension 추가

---
+++ Calculator_Animation_Study/UIView+.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+.swift
+//  Calculator_Animation_Study
+//
+//  Created by 정채은 on 2023/06/12.
+//
+
+import UIKit.UIView
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+}
